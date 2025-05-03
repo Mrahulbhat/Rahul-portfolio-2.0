ca837f0e0100063bf78c16df33b99cfafa15f0ad
Mrahulbhat <rahulbhat5122@gmail.com>
2025-05-03 11:57:34 +0530

    updated experience and project ui reduced mobile card size

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -36,25 +36,13 @@ const About = () => {
         <div className="timeline">
           <div className="timeline-item">
             <div className="timeline-dot"></div>
-            <div className="timeline-date">Feb 2024 - Present</div>
+            <div className="timeline-date">Feb 2024 - Feb 2025</div>
             <div className="timeline-content">
               <h3>Core Committee member</h3>
               <p>Skill Sphere Club · Canara Engineering College</p>
               <p>
                 Conducted sessions to teach problem solving and data structures
                 and algorithms.
-              </p>
-            </div>
-          </div>
-          <div className="timeline-item">
-            <div className="timeline-dot"></div>
-            <div className="timeline-date">Aug 2024 - Nov 2024</div>
-            <div className="timeline-content">
-              <h3>Ex - SDE Intern </h3>
-              <p>Entertainment Technologists · Remote</p>
-              <p>
-                Successfully completed a 3-month internship, gaining valuable
-                industry exposure and experience.
               </p>
             </div>
           </div>
